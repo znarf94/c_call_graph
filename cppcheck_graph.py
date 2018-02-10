5d4ab7d3fecdcfd96ccd8344e22d1388cfff39b2
Franz LAÜGT <znarf94@gmail.com>
2018-02-10 13:21:55 +0100

    GraphViz output

--- cppcheck_graph.py
+++ cppcheck_graph.py
@@ -1,26 +1,32 @@
+import os
 from collections import defaultdict
 from os import walk
 from pprint import pprint
-from subprocess import call
+from subprocess import call, Popen, PIPE
+
 from typing import List
 
 from cppcheckdata import parsedump, Token
 
 CPP_CHECK = '/home/znarf/Téléchargements/cppcheck-1.82/cppcheck'
 
-if __name__ == '__main__':
-    calls = defaultdict(set)
 
-    for _, _, files in walk('.'):
-        for file in files:
+def main():
+    calls = defaultdict(set)
+    for dirpath, _, files in walk('.'):
+        for basename in files:
+            file = os.path.join(dirpath, basename)
+
             if file[-2:] == '.c':
                 print(file)
                 call([CPP_CHECK, '--dump', file])
 
-                d = parsedump(file + '.dump')
+                dump_file = file + '.dump'
+                d = parsedump(dump_file)
+                os.remove(dump_file)
 
-                functions = d.configurations[0].functions
-                scopes = d.configurations[0].scopes
+                # functions = d.configurations[0].functions
+                # scopes = d.configurations[0].scopes
                 tokens = d.configurations[0].tokenlist  # type: List[Token]
 
                 for t in tokens:
@@ -29,3 +35,24 @@ if __name__ == '__main__':
                             calls[t.scope.function.name].add(t.function.name)
 
                 pprint(calls, indent=2)
+
+    dot_graph(calls)
+
+
+def dot_graph(d: defaultdict(set)) -> None:
+    dot = 'digraph d {\n'
+
+    for a, b in d.items():
+        dot += '"{}" -> {{"{}"}};\n'.format(a, '" "'.join(b))
+
+    dot += '}'
+
+    outfile = "graph.png"
+    p = Popen(['dot', '-Tpng', '-o', outfile], stdin=PIPE, stdout=PIPE)
+    p.communicate(bytes(dot, 'utf8'))
+
+    call(['xdg-open', outfile])
+
+
+if __name__ == '__main__':
+    main()
